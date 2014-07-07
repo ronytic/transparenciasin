58e55936c6f232afccfb36fe48c22905d23bc948
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-07-07 11:34:41 -0400

    Corregido Config

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $url="http://localhost/";//ej:http:"localhost/"
 $gestion="Ministerio de Transparencia Institucional y lucha contra la Corrupción";
 $lema='"Ministerio de Transparencia Institucional y lucha contra la Corrupción"';
 
-$ipsoap="http://www.webservicex.net/geoipservice.asmx?WSDL";
+$ipsoap="http://10.1.10.109/WSMT/ServicioMT.asmx?WSDL";
 
 $ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
 $tp="D3W1N8E0E4B4B4";
